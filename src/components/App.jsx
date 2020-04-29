c9a7c3bd25d0bfd4a971f96ce6b6e1ef91def701
AF MIN & MAX <50870007+afminmax@users.noreply.github.com>
2020-04-29 18:39:12 +0200

    added bootstrap and example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import "../styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import "../styles.css";
+import Navigation from './Navigation'
+import Button from 'react-bootstrap/Button'
 
 export default function App() {
   return (
     <div className="App">
+      <Navigation />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+export default function Navigation() {
+  return <button type='button' class='btn btn-danger'>A bootstrap button</button>;
+}
